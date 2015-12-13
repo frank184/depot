a8104ed151081d620d32a85b2bd12cff467258bc
frank184 <francoisbelanger1993@hotmail.com>
2015-12-13 13:09:07 -0500

    iteration B1, B2

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,10 @@
 class Product < ActiveRecord::Base
-  validates :title, :description, :image_url, presence: true
-  validates :price, numericality: { greater_than_or_equal_to: 0.01 }, if: :title_present?
+  validates :title, :description, :price, :image_url, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }, if: 'price.present?'
   validates :image_url, allow_blank: true, format: {
-    with: /\.(gif|jp[e]?g|png)/i,
+    with: /\.(gif|jp[e]?g|png)\z/i,
     message: "Image url must be a URL for a .gif, .jpg, .jpeg or .png"
   }
   validates :title, uniqueness: true
-  def title_present?
-    title.present?
-  end
+
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,55 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+
+  test "product attribute must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test "product title must be unique" do
+    product = new_product
+    product.title = products(:ruby).title
+    assert product.invalid?
+    assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
+  end
+
+  test "product price must be positive" do
+    product = new_product
+    product.price = -1
+    assert product.invalid?
+    count = 0.01
+
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+    product.price = 1
+    assert product.valid?
+  end
+
+  test "product image_url must be of certain types" do
+    ok =  %w{ fred.gif fred.jpg fred.jpeg fred.png
+          FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+
+    ok.each do |url|
+      product = new_product
+      product.image_url = url
+      assert product.valid?, "#{url} should be valid"
+    end
+
+    bad.each do |url|
+      product = new_product
+      product.image_url = url
+      assert product.invalid?, "#{url} should be invalid"
+    end
+  end
+
+  def new_product
+    Product.new(title:"xyz", description:"xyz", price:1, image_url:"xyz.jpg")
+  end
+
 end
