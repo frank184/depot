98754c8696cec4f0e2ccf68b641eec95b960e1b4
frank184 <francoisbelanger1993@hotmail.com>
2015-12-13 03:47:42 -0500

    iteration A1

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,12 @@
 class Product < ActiveRecord::Base
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }, if: :title_present?
+  validates :image_url, allow_blank: true, format: {
+    with: /\.(gif|jp[e]?g|png)/i,
+    message: "Image url must be a URL for a .gif, .jpg, .jpeg or .png"
+  }
+  validates :title, uniqueness: true
+  def title_present?
+    title.present?
+  end
 end
