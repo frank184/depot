7b3644eff49af247aa20cc60fce1cc9eccbd74b4
frank184 <francoisbelanger1993@hotmail.com>
2015-12-15 14:04:44 -0500

    iterations d1,d2,d3: created session based cart and cart line_items

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :price, :image_url, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }, if: 'price.present?'
   validates :image_url, allow_blank: true, format: {
@@ -10,5 +14,15 @@ class Product < ActiveRecord::Base
   def self.latest
     Product.order(:updated_at).last
   end
+
+  private
+
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        true
+      else
+        errors.add(:base, "Line Item present")
+      end
+    end
 
 end
