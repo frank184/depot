2f089a30e75358edc45e518ea86f86559114f7bc
frank184 <francoisbelanger1993@hotmail.com>
2016-01-01 18:15:01 -0500

    iteration J1: selecting the locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,28 @@ class ApplicationController < ActionController::Base
 
   include CurrentUser
 
-  # before_action :set_development_console
-
   before_action :authorize
+  before_action :set_i18n_locale_from_params
+  # before_action :set_development_console
 
   protected
     def authorize
       redirect_to login_path, notice: "Please Log In" unless current_user.present?
+    end
+
+    def set_i18n_locale_from_params
+      if params[:locale]
+        if I18n.available_locales.map(&:to_s).include?(params[:locale])
+          I18n.locale = params[:locale]
+        else
+          flash[:notice] = "#{params[:locale]} translation not available"
+          logger.error flash[:notice]
+        end
+      end
+    end
+
+    def default_url_options
+      { locale: I18n.locale }
     end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,28 @@
 Rails.application.routes.draw do
-  root 'store#index', as: :store
+  scope '(:locale)' do
+    root 'store#index', as: :store
+    get 'contact/index', as: :contact
+    get 'news/index', as: :news
+    get 'questions/index', as: :questions
 
-  scope :admin do
-    get "/", to: "admin#index", as: :admin
-  end
+    scope :admin do
+      get "/", to: "admin#index", as: :admin
+    end
 
-  controller :sessions do
-    get 'login', to: :new
-    post 'login', to: :create
-    delete 'logout', to: :destroy
-  end
+    controller :sessions do
+      get 'login', to: :new
+      post 'login', to: :create
+      delete 'logout', to: :destroy
+    end
 
-  resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  resources :products do
-    get :who_bought, on: :member
+
+    resources :products do
+      get :who_bought, on: :member
+    end
+
+    resources :users
+    resources :orders
+    resources :line_items
+    resources :carts
   end
 end
