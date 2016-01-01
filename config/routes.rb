052f620f41debcbc699c41e96e6fd1bdb42cf67e
frank184 <francoisbelanger1993@hotmail.com>
2016-01-01 16:25:47 -0500

    minor refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   root 'store#index', as: :store
 
+  scope :admin do
+    get "/", to: "admin#index", as: :admin
+  end
+
   controller :sessions do
     get 'login', to: :new
     post 'login', to: :create
     delete 'logout', to: :destroy
   end
 
-  resources :admin
   resources :users
   resources :orders
   resources :line_items
